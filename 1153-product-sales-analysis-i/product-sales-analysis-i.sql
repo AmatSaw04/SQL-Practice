b0b6f4039614b8e5c127625271dd41b9b20ac63d
Amatya Sawant <amatyasawant@gmail.com>
2025-11-02 16:14:09 +0530

    Time: 2634 ms (87.89%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,11 @@
-/* Write your T-SQL query statement below */
-SELECT product_name, year, price 
-From Product as p
-Join Sales as s
-ON p.product_id = s.product_id  
+SELECT 
+    CASE 
+        WHEN s.product_id = p.product_id THEN p.product_name
+        ELSE 'Unknown'
+    END AS product_name,
+    s.year,
+    s.price
+FROM 
+    Sales s
+LEFT JOIN 
+    Product p ON s.product_id = p.product_id
