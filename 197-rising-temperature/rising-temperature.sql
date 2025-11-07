d725ecbcec8914e38731d17ea098286b25ea8495
Amatya Sawant <amatyasawant@gmail.com>
2025-11-07 16:17:56 +0530

    Time: 484 ms (48.64%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,4 @@
-SELECT current_day.id
-FROM Weather AS current_day
-WHERE EXISTS (
-    SELECT 1
-    FROM Weather AS yesterday
-    WHERE current_day.temperature > yesterday.temperature
-    AND current_day.recordDate = DATEADD(day, 1, yesterday.recordDate)
-);
+/* Write your T-SQL query statement below */
+select w2.id from weather w1 
+inner join weather w2
+on DATEDIFF(day, w1.recordDate, w2.recordDate ) = 1 and w2.temperature > w1.temperature
