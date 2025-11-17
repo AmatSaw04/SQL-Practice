ac8f296f6429005d7793badb2a0747a5758092ec
Amatya Sawant <amatyasawant@gmail.com>
2025-11-17 10:18:32 +0530

    Time: 814 ms (13.77%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,19 @@
-SELECT name 
-FROM Employee 
-WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(*) >= 5)
+WITH manager AS (
+    SELECT 
+        id, 
+        name 
+    FROM Employee
+    UNION ALL
+    SELECT 
+        e.id, 
+        e.name
+    FROM Employee e
+    INNER JOIN manager mh 
+        ON e.managerId = mh.id
+)
+SELECT 
+    m.name
+FROM Employee m
+JOIN Employee e ON m.id = e.managerId
+GROUP BY m.id, m.name
+HAVING COUNT(e.id) >= 5;
