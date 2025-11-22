20fc97df6ef01a5e8838166ba7888544a0dffa1f
Amatya Sawant <amatyasawant@gmail.com>
2025-11-22 19:21:50 +0530

    Time: 1106 ms (77.27%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,6 +1,12 @@
 /* Write your T-SQL query statement below */
-select p.project_id, round(avg(e.experience_years * 1.00), 2) average_years 
+with A as 
+(
+select project_id, e.employee_id, experience_years 
 from Project p
 left join Employee e
 on p.employee_id=e.employee_id
-group by p.project_id
+)
+
+select A.project_id, round(avg(experience_years * 1.00), 2) average_years
+from A
+group by A.project_id
