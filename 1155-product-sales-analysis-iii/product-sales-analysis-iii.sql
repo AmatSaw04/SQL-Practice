26783263e10b279813be281e370286208dc332a0
Amatya Sawant <amatyasawant@gmail.com>
2025-11-28 14:26:29 +0530

    Time: 2012 ms (84.43%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,9 +1,3 @@
-/* Write your T-SQL query statement below */
-with res as (
-    select s.product_id, year, quantity, price,
-    Rank() over(partition by s.product_id order by s.product_id, year) rank
-    from Sales s
-)
-select product_id, year first_year, quantity, price
-from res
-where rank = 1
+select s.product_id, s.year first_year , s.quantity, s.price
+FROM Sales s
+WHERE year = (SELECT MIN(year) FROM Sales WHERE product_id = s.product_id);
