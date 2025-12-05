d52a88e748b7864833305a26b349a9a0df19f09a
Amatya Sawant <amatyasawant@gmail.com>
2025-12-05 09:45:27 +0530

    Time: 1058 ms (81.53%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,4 +1,4 @@
-SELECT TOP 1 person_name FROM
-(SELECT person_name,turn, SUM(weight) OVER(ORDER BY turn ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)  AS Total_weight FROM Queue) AS T
-WHERE Total_weight <=1000
-ORDER BY Total_weight DESC
+select top 1 person_name from
+(select person_name,turn, SUM(weight) over(order by turn) Total_weight from Queue) T
+where Total_weight <=1000
+order by Total_weight DESC
